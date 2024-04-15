e564383d3f555fb9ff4c2bf611560152b2161a4c
klchib3 <klchib3@gmail.com>
2024-04-15 20:59:36 +0800

    first version

---
+++ src/main/java/com/example/demo/DemoEnvApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEnvApplication implements CommandLineRunner {
+	
+	@Value("${which.env}")
+	private String env;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEnvApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("env:" + env);
+	}
+
+}
